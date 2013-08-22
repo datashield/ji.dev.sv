48952cc4ff5fd4c8ca68811aaaa1e982c1ae135d
jisaeva <julii@IKBM-B19011444.ans.umb.no>
2013-08-22 10:46:03 +0200

    Datasets are changed

--- test/tests.R
+++ test/tests.R
@@ -5,20 +5,27 @@ library(opal)
 library(opaladmin)
 
 # log in to opals
-o1<-opal.login('administrator', 'password', 'http://54.242.140.255')
-o2<-opal.login('administrator', 'password', 'http://54.242.46.59')
-opals<-list(o1, o2)
-names(opals)<-c('Study 1', 'Study 2')
+server1 <- opal.login('administrator', 'password', 'http://54.242.140.255')
+server2 <- opal.login('administrator', 'password', 'http://54.242.46.59')
+server3 <- opal.login('administrator', 'password', 'http://23.22.215.42')
+opals <- list(server1,server2,server3)
+names(opals) <- c("Study1", "Study2", "Study3")
 
 # assign data to opals
-datashield.assign(opals[[1]], 'D', 'HOPsim.hopsim1ob')
-datashield.assign(opals[[2]], 'D', 'HOPsim.hopsim2ob')
+datashield.assign(server1, 'D', 'CNSIM.CNSIM')
+datashield.assign(server2, 'D', 'CNSIM.CNSIM')
+datashield.assign(server3, 'D', 'CNSIM.CNSIM')
 
 # install respective R package (remove the previous version if needed)
 # dsadmin.remove_package(opals, 'ji.dev.sv')
 dsadmin.install_package(opals, 'ji.dev.sv', ref='master')
 
-# test the density grid function
-datashield.aggregate(opals, quote(ji.griddensitylim.ds(D$sbp, D$glu,x.min=80,x.max=200,y.min=4,y.max=9)))
-datashield.aggregate(opals, quote(ji.griddensitylim.ds(D$sbp, D$glu,x.min=80,x.max=200,y.min=4,y.max=9, numints=15)))
-datashield.aggregate(opals, quote(ji.griddensitylim.ds(D$sbp, D$glu,x.min=80,x.max=200,y.min=4,y.max=9, numints=25)))
+# generate a density grid matrix
+datashield.aggregate(opals, quote(ag.densitygrid.ds(D$LAB_TSC, D$LAB_HDL)))
+
+# generate a density grid matrix with defined limits
+datashield.aggregate(opals, quote(ji.griddensitylim.ds(D$LAB_TSC, D$LAB_HDL,x.min=1,x.max=11,y.min=0,y.max=3)))
+datashield.aggregate(opals, quote(ji.griddensitylim.ds(D$LAB_TSC, D$LAB_HDL,x.min=1,x.max=11,y.min=4,y.max=3, numints=15)))
+datashield.aggregate(opals, quote(ji.griddensitylim.ds(D$LAB_TSC, D$LAB_HDL,x.min=1,x.max=11,y.min=0,y.max=3, numints=25)))
+
+
