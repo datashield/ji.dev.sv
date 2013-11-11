76ba00d88e3de1e2ff9866f89a6bf88bb5dc0dcf
jisaeva <juis@PC-60079.fhi.no>
2013-11-11 10:33:50 +0100

    Amended example

--- R/ji.densitygrid.ds.R
+++ R/ji.densitygrid.ds.R
@@ -13,6 +13,19 @@
 #' @return a grid density matrix
 #' @author Isaeva, J. and Gaye, A.
 #' @export
+#' @examples 
+#' \dontrun{
+#' # load the file that contains the login details
+#' library(opal)
+#' data(logindata)
+#' 
+#  # login and assign a numeric variable to R
+#  myvar <- list("LAB_TSC")
+#' opals <- datashield.login(logins=logindata,assign=TRUE,variables=myvar)
+#' 
+#' # compute the statistical mean
+#' stat.mean <- datashield.aggregate(opals, quote(mean.ds(D$LAB_TSC)))
+#' }
 #' 
 ji.densitygrid.ds  <-  function(xvect,yvect, limits=FALSE, x.min=NULL, x.max=NULL, y.min=NULL, y.max=NULL, numints=20){
   
