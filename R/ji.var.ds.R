de095c1572800734e29a63d6fbc68a0704b3fab0
jisaeva <juis@PC-60079.fhi.no>
2014-02-12 14:04:09 +0100

    Amended examples and input parameters

--- R/ji.var.ds.R
+++ R/ji.var.ds.R
@@ -1,7 +1,8 @@
 #'
 #' @title Computes the variance of a vector or matrix
 #' @param x a numeric vector, matrix or data frame.
-#' @return a single value if \code{x} is a vector and a covariance matrix if \code{x} is a matrix
+#' @param y \code{NULL} (default) or a vector, matrix or data frame with compatible dimensions to \code{x}.
+#' @return a single value if \code{x} and \code{y} are vectors and a matrix otherwise
 #' @author Isaeva, J.
 #' @export
 #' @examples 
@@ -18,14 +19,15 @@
 #' stat.var <- datashield.aggregate(opals, quote(ji.var.ds(D$LAB_TSC)))
 #' 
 #' # compute the variance of matrix (LAB_TSC,LAB_TRIG)
-#' stat.var <- datashield.aggregate(opals, quote(ji.var.ds(cbind(D$LAB_TSC,D$LAB_TRIG)))
+#' datashield.assign(opals,'dummy', quote(cbind(D$LAB_TSC,D$LAB_TRIG)))
+#' stat.var <- datashield.aggregate(opals, quote(ji.var.ds(dummy)))
 #' }
 #'
-ji.var.ds <- function (x) {
+ji.var.ds <- function (x, y=NULL) {
 #   if(length(xvect) > 0 & length(xvect) < 5){
 #     stop("Operation not allowed: argument contains between 1 and 4 observations only!\n")
 #   }else{
-    result <- var(x, na.rm=TRUE) 
+    result <- var(x, y, na.rm=TRUE) 
 #   }
   return(result)
 }
